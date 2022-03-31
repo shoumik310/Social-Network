619e3a158143a518852e349c115f5885be481fa7
Shoumik Panandikar <shoumikrp2000@gmail.com>
2022-03-31 20:04:24 +0530

    Added Redux Store

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
-function App() {
-	return (
-		<Fragment>
-			<h1>App</h1>
-		</Fragment>
-	);
-}
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+const App = () => (
+	<Provider store={store}>
+		<Router>
+			<Fragment>
+				<Navbar />
+				<Routes>
+					<Route exact path='/' element={<Landing />} />
+				</Routes>
+				<section className='container'>
+					<Routes>
+						<Route exact path='/register' element={<Register />} />
+						<Route exact path='/login' element={<Login />} />
+					</Routes>
+				</section>
+			</Fragment>
+		</Router>
+	</Provider>
+);
 
 export default App;
