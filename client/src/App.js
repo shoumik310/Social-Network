974fb8fba84866a1757034962eec41bd28f705f2
Shoumik Panandikar <shoumikrp2000@gmail.com>
2022-04-02 14:21:27 +0530

    Added redux for alerts

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
 import './App.css';
 
 //Redux
@@ -19,6 +20,7 @@ const App = () => (
 					<Route exact path='/' element={<Landing />} />
 				</Routes>
 				<section className='container'>
+					<Alert />
 					<Routes>
 						<Route exact path='/register' element={<Register />} />
 						<Route exact path='/login' element={<Login />} />
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,3 +1,4 @@
 import { combineReducers } from 'redux';
+import alertReducer from './alert';
 
-export default combineReducers({});
+export default combineReducers({ alertReducer });
